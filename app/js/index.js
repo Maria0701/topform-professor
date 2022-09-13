7c6cde065712917e80a133f43f8ddba2599b14c7
Maria0701 <reflection2008u@gmail.com>
2022-09-13 18:01:58 +0300

    feat: slider finished

--- app/js/index.js
+++ app/js/index.js
@@ -10,177 +10,7 @@ import { menuOpener } from './components/menu-opener';
 
 const { swiperMode } = require("./components/btns-swiper");
 const { tabsOpener } = require("./components/tabs");
-/*
-try {
-document.addEventListener("DOMContentLoaded", () => {
 
-	const header = document.querySelector(".header:not(.header--fixed)");
-	const heightHeader = header.offsetHeight;
-	const main = document.querySelector(".main");
-
-	headerFixed();
-
-	window.addEventListener("scroll", headerFixed, false);
-
-	function headerFixed() {
-		const heightOffset = window.pageYOffset;
-
-		if (heightOffset > 0) {
-			main.style.paddingTop = `${heightHeader}px`;
-			header.classList.add("header--fixed");
-		} else if(heightOffset == 0) {
-			main.style.paddingTop = 0;
-			header.classList.remove("header--fixed");
-		}
-	}
-
-	const swiperHero = new Swiper('.hero-slider', {
-		slidesPerView: 1,
-		pagination: {
-			el: ".hero .swiper-pagination",
-		},
-		navigation: {
-			nextEl: ".hero .swiper-button-next",
-			prevEl: ".hero .swiper-button-prev",
-		},
-	});
-
-	const swiperStock = new Swiper('.stock-slider', {
-		loop: true,
-        spaceBetween: 16,
-		slidesPerView: "auto",
-		navigation: {
-			nextEl: ".stock .swiper-button-next",
-			prevEl: ".stock .swiper-button-prev",
-		},
-		breakpoints: {
-			640: {
-				spaceBetween: 53,
-			}
-		}
-	});
-
-	const swiperSpec = new Swiper('.spec-slider', {
-        spaceBetween: 16,
-		slidesPerView: "auto",
-		navigation: {
-			nextEl: ".benefits .swiper-button-next",
-			prevEl: ".benefits .swiper-button-prev",
-		},
-		breakpoints: {
-			640: {
-				spaceBetween: 53,
-				slidesPerView: 4,
-			}
-		}
-	});
-
-	const swiperReviews = new Swiper('.reviews-slider', {
-		loop: true,
-		slidesPerView: 1,
-		pagination: {
-			el: ".reviews .swiper-pagination",
-			type: "fraction",
-		},
-		navigation: {
-			nextEl: ".reviews .swiper-button-next",
-			prevEl: ".reviews .swiper-button-prev",
-		},
-	});
-
-	(function() {
-		const btnMenuMobile = document.querySelector(".js-menu-mobile-button");
-		const menuMobile = document.querySelector(".js-menu-mobile")
-		const iconMenu = document.querySelector(".icon-menu")
-		const iconCancel = document.querySelector(".icon-cancel")
-		let isOpened = false;
-
-		btnMenuMobile.addEventListener("touchend", (event)=>{
-			if (isOpened) {
-				menuMobile.classList.remove("is-open");
-				iconMenu.style.display = "block";
-				iconCancel.style.display = "none";
-				isOpened = false;
-			} else {
-				menuMobile.classList.add("is-open");
-				iconMenu.style.display = "none";
-				iconCancel.style.display = "block";
-				isOpened = true;
-			}
-		});
-	})();
-
-	(function() {
-		const headerInnerItems = document.querySelectorAll(".header-menu__item--inner");
-		const innerMenus = document.querySelectorAll(".inner-menu");
-		const headerWrapper = document.querySelector(".header-menu__wrap");
-		const headerBack = document.querySelector(".header-menu-back");
-		const headerBackBtn = document.querySelector(".btn-menu-back");
-
-		headerInnerItems.forEach((item) => {
-			item.addEventListener("touchend", function(event) {
-				event.stopPropagation();
-
-				const innerMenu = event.currentTarget.querySelector(".inner-menu");
-				const heightInnerMenu = innerMenu.offsetHeight;
-
-				headerWrapper.style.height = `${heightInnerMenu}px`;
-				headerBack.style.display = "block";
-			});
-		});
-
-		headerBackBtn.addEventListener("touchend", function(event) {
-			headerWrapper.style.height = "auto";
-			innerMenus.forEach((innerMenu) => {
-				innerMenu.classList.remove("is-show");
-			});
-			headerBack.style.display = "none";
-			event.stopPropagation();
-		}, false);
-	})();
-
-	(function() {
-		const headerInnerItem = document.querySelector(".header-menu__item--inner");
-
-		headerInnerItem.addEventListener("mouseenter", (event)=>{
-			const innerMenu = event.currentTarget.querySelector(".inner-menu");
-			innerMenu.classList.add("is-show");
-		});
-		headerInnerItem.addEventListener("touchend", (event)=>{
-			if (event.currentTarget.classList.contains("header-menu__item--inner")) {
-				const innerMenu = event.currentTarget.querySelector(".inner-menu");
-				innerMenu.classList.add("is-show");
-			}
-		});
-		headerInnerItem.addEventListener("mouseleave", (event)=>{
-			const innerMenu = event.currentTarget.querySelector(".inner-menu");
-			innerMenu.classList.remove("is-show");
-		});
-	})();
-
-	(function() {
-		const buttons = document.querySelectorAll(".js-service-tab-item");
-		const sections = document.querySelectorAll(".js-service-tab-content");
-
-		buttons.forEach((btn)=> {
-			btn.addEventListener("click", ()=>{
-				buttons.forEach((btn)=>{
-					btn.classList.remove("is-active");
-				});
-				btn.classList.add("is-active");
-				const tabName = btn.id;
-				sections.forEach((section)=>{
-					section.classList.remove("is-active");
-				});
-				const req = document.getElementsByClassName(`${tabName}`);
-				req[0].classList.add("is-active");
-			})
-		})
-	})();
-});
-} catch(e) {
-	console.log(e);
-}*/
 try {
 	tabsOpener('.js-tabs');
 } catch(e) {
@@ -298,6 +128,21 @@ try {
 		},
 	};
 
+	const optionsHero = {
+		modules: [ Navigation, Pagination],
+		slidesPerView: 1,
+		loop: true,
+		observer: true,
+		observeParents: true,
+		navigation: {
+			nextEl: '.hero__right',
+			prevEl: '.hero__left',
+		},
+		pagination: {
+			el: ".hero-slider__pagination",
+		},
+	};
+
 	const optionsHistory = {
 		slidesPerView: "auto",
         spaceBetween: 16,
@@ -318,8 +163,8 @@ try {
 	const swiperDoctors = new Swiper('.doctors__swiper', optionsDoctors);
 	const swiperPromo = new Swiper('.promo-block__swiper', optionsPromo);
 	const swiperReviews = new Swiper('.review-slider__swiper', optionsReviews);	  
-	const swiperHistory = new Swiper('.history__swiper', optionsHistory);	  
-
+	const swiperHistory = new Swiper('.history__swiper', optionsHistory);
+	const swiperHero = new Swiper('.hero-slider__swiper', optionsHero);
 } catch(e) {
 	console.log(e)
 }
--- build/js/index.js
+++ build/js/index.js
@@ -13937,178 +13937,6 @@ var _require = __webpack_require__(405),
 
 var _require2 = __webpack_require__(291),
     tabsOpener = _require2.tabsOpener;
-/*
-try {
-document.addEventListener("DOMContentLoaded", () => {
-
-	const header = document.querySelector(".header:not(.header--fixed)");
-	const heightHeader = header.offsetHeight;
-	const main = document.querySelector(".main");
-
-	headerFixed();
-
-	window.addEventListener("scroll", headerFixed, false);
-
-	function headerFixed() {
-		const heightOffset = window.pageYOffset;
-
-		if (heightOffset > 0) {
-			main.style.paddingTop = `${heightHeader}px`;
-			header.classList.add("header--fixed");
-		} else if(heightOffset == 0) {
-			main.style.paddingTop = 0;
-			header.classList.remove("header--fixed");
-		}
-	}
-
-	const swiperHero = new Swiper('.hero-slider', {
-		slidesPerView: 1,
-		pagination: {
-			el: ".hero .swiper-pagination",
-		},
-		navigation: {
-			nextEl: ".hero .swiper-button-next",
-			prevEl: ".hero .swiper-button-prev",
-		},
-	});
-
-	const swiperStock = new Swiper('.stock-slider', {
-		loop: true,
-        spaceBetween: 16,
-		slidesPerView: "auto",
-		navigation: {
-			nextEl: ".stock .swiper-button-next",
-			prevEl: ".stock .swiper-button-prev",
-		},
-		breakpoints: {
-			640: {
-				spaceBetween: 53,
-			}
-		}
-	});
-
-	const swiperSpec = new Swiper('.spec-slider', {
-        spaceBetween: 16,
-		slidesPerView: "auto",
-		navigation: {
-			nextEl: ".benefits .swiper-button-next",
-			prevEl: ".benefits .swiper-button-prev",
-		},
-		breakpoints: {
-			640: {
-				spaceBetween: 53,
-				slidesPerView: 4,
-			}
-		}
-	});
-
-	const swiperReviews = new Swiper('.reviews-slider', {
-		loop: true,
-		slidesPerView: 1,
-		pagination: {
-			el: ".reviews .swiper-pagination",
-			type: "fraction",
-		},
-		navigation: {
-			nextEl: ".reviews .swiper-button-next",
-			prevEl: ".reviews .swiper-button-prev",
-		},
-	});
-
-	(function() {
-		const btnMenuMobile = document.querySelector(".js-menu-mobile-button");
-		const menuMobile = document.querySelector(".js-menu-mobile")
-		const iconMenu = document.querySelector(".icon-menu")
-		const iconCancel = document.querySelector(".icon-cancel")
-		let isOpened = false;
-
-		btnMenuMobile.addEventListener("touchend", (event)=>{
-			if (isOpened) {
-				menuMobile.classList.remove("is-open");
-				iconMenu.style.display = "block";
-				iconCancel.style.display = "none";
-				isOpened = false;
-			} else {
-				menuMobile.classList.add("is-open");
-				iconMenu.style.display = "none";
-				iconCancel.style.display = "block";
-				isOpened = true;
-			}
-		});
-	})();
-
-	(function() {
-		const headerInnerItems = document.querySelectorAll(".header-menu__item--inner");
-		const innerMenus = document.querySelectorAll(".inner-menu");
-		const headerWrapper = document.querySelector(".header-menu__wrap");
-		const headerBack = document.querySelector(".header-menu-back");
-		const headerBackBtn = document.querySelector(".btn-menu-back");
-
-		headerInnerItems.forEach((item) => {
-			item.addEventListener("touchend", function(event) {
-				event.stopPropagation();
-
-				const innerMenu = event.currentTarget.querySelector(".inner-menu");
-				const heightInnerMenu = innerMenu.offsetHeight;
-
-				headerWrapper.style.height = `${heightInnerMenu}px`;
-				headerBack.style.display = "block";
-			});
-		});
-
-		headerBackBtn.addEventListener("touchend", function(event) {
-			headerWrapper.style.height = "auto";
-			innerMenus.forEach((innerMenu) => {
-				innerMenu.classList.remove("is-show");
-			});
-			headerBack.style.display = "none";
-			event.stopPropagation();
-		}, false);
-	})();
-
-	(function() {
-		const headerInnerItem = document.querySelector(".header-menu__item--inner");
-
-		headerInnerItem.addEventListener("mouseenter", (event)=>{
-			const innerMenu = event.currentTarget.querySelector(".inner-menu");
-			innerMenu.classList.add("is-show");
-		});
-		headerInnerItem.addEventListener("touchend", (event)=>{
-			if (event.currentTarget.classList.contains("header-menu__item--inner")) {
-				const innerMenu = event.currentTarget.querySelector(".inner-menu");
-				innerMenu.classList.add("is-show");
-			}
-		});
-		headerInnerItem.addEventListener("mouseleave", (event)=>{
-			const innerMenu = event.currentTarget.querySelector(".inner-menu");
-			innerMenu.classList.remove("is-show");
-		});
-	})();
-
-	(function() {
-		const buttons = document.querySelectorAll(".js-service-tab-item");
-		const sections = document.querySelectorAll(".js-service-tab-content");
-
-		buttons.forEach((btn)=> {
-			btn.addEventListener("click", ()=>{
-				buttons.forEach((btn)=>{
-					btn.classList.remove("is-active");
-				});
-				btn.classList.add("is-active");
-				const tabName = btn.id;
-				sections.forEach((section)=>{
-					section.classList.remove("is-active");
-				});
-				const req = document.getElementsByClassName(`${tabName}`);
-				req[0].classList.add("is-active");
-			})
-		})
-	})();
-});
-} catch(e) {
-	console.log(e);
-}*/
-
 
 try {
   tabsOpener('.js-tabs');
@@ -14218,6 +14046,20 @@ try {
       type: "fraction"
     }
   };
+  var optionsHero = {
+    modules: [swiper_esm/* Navigation */.W_, swiper_esm/* Pagination */.tl],
+    slidesPerView: 1,
+    loop: true,
+    observer: true,
+    observeParents: true,
+    navigation: {
+      nextEl: '.hero__right',
+      prevEl: '.hero__left'
+    },
+    pagination: {
+      el: ".hero-slider__pagination"
+    }
+  };
   var optionsHistory = {
     slidesPerView: "auto",
     spaceBetween: 16,
@@ -14238,6 +14080,7 @@ try {
   var swiperPromo = new swiper_esm/* default */.ZP('.promo-block__swiper', optionsPromo);
   var swiperReviews = new swiper_esm/* default */.ZP('.review-slider__swiper', optionsReviews);
   var swiperHistory = new swiper_esm/* default */.ZP('.history__swiper', optionsHistory);
+  var swiperHero = new swiper_esm/* default */.ZP('.hero-slider__swiper', optionsHero);
 } catch (e) {
   console.log(e);
 } // обрезает текст комментариев
